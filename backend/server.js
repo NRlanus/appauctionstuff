0126abb349cad8a910c222d974687a8a1c0ec272
Nico <niklanus@gmail.com>
2024-03-27 15:21:39 -0300

    cambios en el server para usar sqlite3

--- backend/server.js
+++ backend/server.js
@@ -2,103 +2,143 @@ const mysql = require('mysql2');
 const express = require('express');
 const cors = require('cors');
 
+const sqlite3 = require('sqlite3').verbose();
+
 const app = express();
 const port = 3001;
 
 app.use(cors());
 app.use(express.json());
+app.on('ready', () => {
+  const dbPath = path.join(app.getPath('userData'), 'mydatabase.db');
+  const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+      if (err) {
+          console.error('Error al abrir la base de datos', err.message);
+      } else {
+          console.log('Conectado a la base de datos SQLite.');
+          initializeDatabase();
+      }
+  });
+});
 
-const db = mysql.createPool({
+ 
+function initializeDatabase() {
+  db.run(`CREATE TABLE IF NOT EXISTS items (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ItemNumber TEXT,
+    Name TEXT,
+    PurchasePrice REAL,
+    SoldPrice REAL,
+    Notes TEXT,
+    Retail REAL,
+    MonthlyInPurchase TEXT,
+    DateSold TEXT,
+    Site TEXT
+  )`);
+
+  db.run(`CREATE TABLE IF NOT EXISTS fees (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    InvoiceId TEXT,
+    Premium REAL,
+    Freight REAL,
+    Tax REAL
+  )`);
+
+  db.run(`CREATE TABLE IF NOT EXISTS numbers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Number TEXT,
+    Description TEXT
+  )`);
+
+  // Puedes continuar aquí con la creación de otras tablas necesarias.
+  console.log("Tablas inicializadas correctamente.");
+}
+/*const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'Admin1234',
   database: 'auctionstuff'
-});
+});*/
+
 
 
   //================GET ITEMS LIST================
 app.get('/api/items', (req, res) => {
-    const sqlSelect = "SELECT * FROM items";
-    db.query(sqlSelect, (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Error al obtener los datos');
-      } else {
-        res.send(result);
-      }
-    });
+  const sqlSelect = "SELECT * FROM items";
+  db.all(sqlSelect, [], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error al obtener los datos');
+    } else {
+      res.send(result);
+    }
   });
+});
 
   //================GET NUMBERS LIST================
-  app.get('/api/numbers', (req, res) => {
-    const sqlSelect = "SELECT * FROM numbers";
-    db.query(sqlSelect, (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error al obtener los datos de la segunda tabla');
-      } else {
-        res.send(result);
-      }
-    });
+app.get('/api/numbers', (req, res) => {
+  const sqlSelect = "SELECT * FROM numbers";
+  db.all(sqlSelect, [], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error al obtener los datos de la segunda tabla');
+    } else {
+      res.send(result);
+    }
   });
-  
-  app.get('/api/fees', (req, res) => {
-    const sqlSelect = "SELECT * FROM fees";
-    db.query(sqlSelect, (err, result) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error al obtener los datos de la segunda tabla');
-      } else {
-        res.send(result);
-      }
-    });
+});
+app.get('/api/fees', (req, res) => {
+  const sqlSelect = "SELECT * FROM fees";
+  db.all(sqlSelect, [], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error al obtener los datos de la segunda tabla');
+    } else {
+      res.send(result);
+    }
   });
+});
   
 //===================ADD NEW ITEMS=======================
 app.post('/api/add-item', (req, res) => {
-    // Ejemplo de cómo podrías insertar datos en la tabla 'items'
-    const { itemNumber, name, purchasePrice, soldPrice, notes, retail, monthlyInPurchase, dateSold, site } = req.body; // Asegúrate de validar y sanitizar los datos de entrada
+    const { itemNumber, name, purchasePrice, soldPrice, notes, retail, monthlyInPurchase, dateSold, site } = req.body;
     const sqlInsert = "INSERT INTO items (ItemNumber, Name, PurchasePrice, SoldPrice, Notes, Retail, MonthlyInPurchase, DateSold, Site) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    db.query(sqlInsert, [itemNumber, name, purchasePrice, soldPrice, notes, retail, monthlyInPurchase, dateSold, site], (err, result) => {
+    db.run(sqlInsert, [itemNumber, name, purchasePrice, soldPrice, notes, retail, monthlyInPurchase, dateSold, site], function(err) {
       if (err) {
         console.error("Error al insertar en 'items':", err);
         res.status(500).send('Error al agregar el item');
       } else {
-        res.status(200).send('Item agregado correctamente');
+        res.status(200).send(`Item agregado correctamente con el ID: ${this.lastID}`);
       }
     });
+});
+
+app.post('/api/add-fees', (req, res) => {
+  const { invoiceId, premium, freight, tax } = req.body;
+  const sqlInsert = "INSERT INTO fees (InvoiceId, Premium, Freight, Tax) VALUES (?, ?, ?, ?)";
+  db.run(sqlInsert, [invoiceId, premium, freight, tax], function(err) {
+    if (err) {
+      console.error("Error al insertar en 'fees':", err);
+      res.status(500).send('Error al agregar el fee');
+    } else {
+      res.status(200).send(`Fee agregado correctamente con el ID: ${this.lastID}`);
+    }
   });
-  app.post('/api/add-fees', (req, res) => {
-    // Ejemplo de cómo podrías insertar datos en la tabla 'items'
-    const { invoiceId, premium, freight, tax  } = req.body; // Asegúrate de validar y sanitizar los datos de entrada
-    const sqlInsert = "INSERT INTO fees (InvoiceId, Premium, Freight, Tax) VALUES (?, ?, ?, ?)";
-    db.query(sqlInsert, [invoiceId, premium, freight, tax], (err, result) => {
-      if (err) {
-        console.error("Error al insertar en 'fees':", err);
-        res.status(500).send('Error al agregar el item');
-      } else {
-        res.status(200).send('Item agregado correctamente');
-      }
-    });
-  });
+});
 
   //===================UPDATE ITEMS=================================
   app.patch('/api/update-item/:id', (req, res) => {
-    const  id  = req.params.id;
+    const id = req.params.id;
     const updates = req.body;
-  console.log(req.body);
-  console.log('id: ', req.params.id);
-    // Inicializar partes de la consulta SQL
     let query = 'UPDATE items SET ';
     const queryParams = [];
   
-    // Construir dinámicamente la consulta SQL basada en los campos proporcionados
     Object.entries(updates).forEach(([key, value], index) => {
-      if (value != null) { // Asegúrate de incluir solo campos que no sean null
+      if (value != null) {
         query += `${key} = ?`;
         queryParams.push(value);
   
-        if (index < Object.keys(updates).length - 1) {
+        if (index < Object.entries(updates).length - 1) {
           query += ", ";
         }
       }
@@ -107,40 +147,32 @@ app.post('/api/add-item', (req, res) => {
     query += ` WHERE id = ?`;
     queryParams.push(id);
   
-    // Verificar que al menos un campo ha sido proporcionado
-    if (queryParams.length === 1) { // Solo el ID está presente, ningún campo a actualizar
-      return res.status(400).send({ error: 'No se proporcionaron campos para actualizar.' });
+    if (queryParams.length <= 1) { // Incluyendo el ID
+      res.status(400).send({ error: 'No se proporcionaron campos para actualizar.' });
+    } else {
+      db.run(query, queryParams, function(err) {
+        if (err) {
+          console.error("Error al actualizar 'items':", err);
+          res.status(500).send('Error al actualizar el item');
+        } else {
+          res.status(200).send(`Item actualizado correctamente. Filas afectadas: ${this.changes}`);
+        }
+      });
     }
-  
-    // Ejecutar la consulta
-    db.query(query, queryParams, (err, result) => {
-      if (err) {
-        console.error("Error al actualizar 'items':", err);
-        res.status(500).send('Error al actualizar el item');
-      } else {
-        res.status(200).send('Item actualizado correctamente');
-      }
-    });
   });
   
-
-  //===================UPDATE FEES=================================
   app.patch('/api/update-fees/:id', (req, res) => {
-    const  id  = req.params.id;
+    const id = req.params.id;
     const updates = req.body;
-  console.log(req.body);
-  console.log('id: ', req.params.id);
-    // Inicializar partes de la consulta SQL
     let query = 'UPDATE fees SET ';
     const queryParams = [];
   
-    // Construir dinámicamente la consulta SQL basada en los campos proporcionados
     Object.entries(updates).forEach(([key, value], index) => {
-      if (value != null) { // Asegúrate de incluir solo campos que no sean null
+      if (value != null) {
         query += `${key} = ?`;
         queryParams.push(value);
   
-        if (index < Object.keys(updates).length - 1) {
+        if (index < Object.entries(updates).length - 1) {
           query += ", ";
         }
       }
@@ -149,20 +181,18 @@ app.post('/api/add-item', (req, res) => {
     query += ` WHERE id = ?`;
     queryParams.push(id);
   
-    // Verificar que al menos un campo ha sido proporcionado
-    if (queryParams.length === 1) { // Solo el ID está presente, ningún campo a actualizar
-      return res.status(400).send({ error: 'No se proporcionaron campos para actualizar.' });
+    if (queryParams.length <= 1) {
+      res.status(400).send({ error: 'No se proporcionaron campos para actualizar.' });
+    } else {
+      db.run(query, queryParams, function(err) {
+        if (err) {
+          console.error("Error al actualizar 'fees':", err);
+          res.status(500).send('Error al actualizar el fee');
+        } else {
+          res.status(200).send(`Fee actualizado correctamente. Filas afectadas: ${this.changes}`);
+        }
+      });
     }
-  
-    // Ejecutar la consulta
-    db.query(query, queryParams, (err, result) => {
-      if (err) {
-        console.error("Error al actualizar 'items':", err);
-        res.status(500).send('Error al actualizar el fee');
-      } else {
-        res.status(200).send('Item actualizado correctamente');
-      }
-    });
   });
   
 
